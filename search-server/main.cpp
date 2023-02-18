8a8d5c34396a621960c03b2b9a270ae6baea0dee
vlad <vlad5956@gmail.com>
2023-02-18 17:01:18 +0300

    Классы для вывода результатов страницами

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -300,20 +300,79 @@ private:
     }
 };
 
-// ==================== для примера =========================
+template <typename iterator>
+class IteratorRange {
+public:
+    iterator begin;
+    iterator end;
+    //int size = 0;
+};
 
-void PrintDocument(const Document& document) {
-    cout << "{ "s
-        << "document_id = "s << document.id << ", "s
-        << "relevance = "s << document.relevance << ", "s
-        << "rating = "s << document.rating << " }"s << endl;
+
+ostream& operator<<(ostream& os, Document value) {
+    os << "{ document_id = " << value.id << ", relevance = " << value.relevance << ", rating = " << value.rating << " }";
+    return os;
 }
-int main() {
-    SearchServer s;
-    try {
-        s.GetDocumentId(-1);
+template <typename iterator>
+ostream& operator<<(ostream& os, IteratorRange<iterator> value) {
+    for (; value.begin < value.end; ++value.begin) {
+        os << *value.begin;
     }
-    catch (out_of_range e) {
-        cout << e.what();
+    return os;
+}
+
+template <typename Iterator>
+class Paginator {
+public:
+    Paginator(Iterator start, Iterator end, int page_size) :
+        start_(start), end_(end), page_size_(page_size){
+        IteratorRange<Iterator> buffer;
+        buffer.begin = start;
+        for (int i = 1; start < end; ++i, ++start) {
+            if (i % page_size == 0) {
+                buffer.end = start + 1;
+                //buffer.size = distance(buffer.begin, buffer.end);
+                data.push_back(buffer);
+                buffer = IteratorRange<Iterator>();
+                buffer.begin = start + 1;
+            }
+        }
+        buffer.end = end;
+        if(buffer.begin != buffer.end) data.push_back(buffer);
+    }
+
+    auto begin() const{
+        return data.begin();
+    }
+
+    auto end() const {
+        return data.end();
+    }
+private:
+    vector<IteratorRange<Iterator>> data;
+    Iterator start_;
+    Iterator end_;
+    int page_size_;
+};
+
+template <typename Container>
+auto Paginate(const Container& c, size_t page_size) {
+    return Paginator(begin(c), end(c), page_size);
+}
+
+int main() {
+    SearchServer search_server("and with"s);
+    search_server.AddDocument(1, "funny pet and nasty rat"s, DocumentStatus::ACTUAL, { 7, 2, 7 });
+    search_server.AddDocument(2, "funny pet with curly hair"s, DocumentStatus::ACTUAL, { 1, 2, 3 });
+    search_server.AddDocument(3, "big cat nasty hair"s, DocumentStatus::ACTUAL, { 1, 2, 8 });
+    search_server.AddDocument(4, "big dog cat Vladislav"s, DocumentStatus::ACTUAL, { 1, 3, 2 });
+    search_server.AddDocument(5, "big dog hamster Borya"s, DocumentStatus::ACTUAL, { 1, 1, 1 });
+    const auto search_results = search_server.FindTopDocuments("curly dog"s);
+    int page_size = 2;
+    const auto pages = Paginate(search_results, page_size);
+    // Выводим найденные документы по страницам
+    for (auto page = pages.begin(); page != pages.end(); ++page) {
+        cout << *page << endl;
+        cout << "Page break"s << endl;
     }
 }
